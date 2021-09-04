6ca973230acbd0a026409e3dfecdce882d99fa45
Gustavo Henrique <gustavohsdp@gmail.com>
2021-09-04 00:53:17 -0300

    adding and configuring miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
+import { App } from "./App";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Desenvolvimento de app Mobile",
+          amount: 12000,
+          type: "deposit",
+          category: "project",
+          createdAt: new Date().toDateString,
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
