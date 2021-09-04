ff8805af6e756bfa07a61ec7830e821e7e58d8c9
Gustavo Henrique <gustavohsdp@gmail.com>
2021-09-04 00:23:32 -0300

    Create component TransactionsTable

---
+++ src/components/Transactions/index.tsx
@@ -0,0 +1,34 @@
+import { Container } from "./styles";
+
+export default function Transactions() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td className="title">Desenvolvimento de app Mobile</td>
+            <td className="deposit">R$ 12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">-R$500</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
