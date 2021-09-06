51e47a9f1be4ca0c5e7cd0a4962a0b0f37dd418d
Gustavo Henrique <gustavohsdp@gmail.com>
2021-09-06 18:16:48 -0300

    transactions

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,8 +1,9 @@
 import styled from "styled-components";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.form`
   h2 {
-    color: var(--text-title-color);
+    color: var(--text-title);
     font-size: 1.5rem;
     margin-bottom: 2rem;
   }
@@ -40,10 +41,60 @@ export const Container = styled.form`
     margin-top: 1.5rem;
     font-weight: 600;
 
-    transition: filter 0.2;
+    transition: filter 0.2s;
 
     &:hover {
       filter: brightness(0.9);
     }
+  }
+`;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 0.5rem;
+`;
+
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: "green" | "red";
+}
+
+const colors = {
+  green: "#33cc95",
+  red: "#e52e4d",
+};
+
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+
+  background: ${(props) => props.isActive
+    ? transparentize(0.9, colors[props.activeColor])
+    : "transparent"
+  };
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: border-color 0.2s;
+
+  &:hover {
+    border-color: ${darken(0.1, "#D7D7D7")};
+  }
+
+  img {
+    height: 20px;
+    width: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `;
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 import { App } from "./App";
 
 createServer({
+  models: {
+    transactions: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-09-06 09:00:26'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1200,
+          createdAt: new Date('2021-09-10 14:00:26'),
+        },
+      ],
+    })
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "Desenvolvimento de app Mobile",
-          amount: 12000,
-          type: "deposit",
-          category: "project",
-          createdAt: new Date().toDateString,
-        },
-      ];
+      return this.schema.all('transactions');
     });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transactions', data);
+    })
   },
 });
 
